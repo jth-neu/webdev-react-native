a461ef996aeb36a2187278faf4ab75ff2c4d4f4a
Jingtian Huang <huang.jin@husky.neu.edu>
2018-06-25 17:21:43 -0700

    Add dependencies and implement home page

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, View } from 'react-native';
+import {createStackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+class Home extends React.Component {
+    static navigationOptions = {
+        title: 'Home'
+    }
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return(
+            <View>
+                <Button title="Courses"/>
+
+            </View>
+        )
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const App = createStackNavigator({
+    Home
 });
+
+export default App;
