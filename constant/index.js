4ffce5d8d32a9cfcc12f7f7fb5cecfed7b148911
Jingtian Huang <huang.jin@husky.neu.edu>
2018-06-26 23:35:58 -0700

    Implement Add essay question in exam widget

---
+++ constant/index.js
@@ -0,0 +1,30 @@
+export const FillInBlanks = {
+    "title": "New Fill in the Blanks",
+    "description": "Description for Fill in the Blanks Question",
+    "points": 0,
+    "variables": "",
+    "type": "blanks"
+};
+
+export const MultipleChoice = {
+    "title": "New Multiple Choice",
+    "description": "Description for Multiple Choice Question",
+    "points": 0,
+    "choices": "",
+    "type": "choice"
+}
+
+export const Essay = {
+    "title": "New Essay",
+    "description": "Description for Essay Question",
+    "points": 0,
+    "type": "essay"
+}
+
+export const TrueFalse = {
+    "title": "New True False",
+    "description": "Description for True False Question",
+    "points": 0,
+    "isTrue": true,
+    "type": "truefalse"
+}
--- elements/ExamWidget.js
+++ elements/ExamWidget.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
-import {ScrollView, View, TextInput, Button, Alert} from 'react-native'
-import {Text, FormLabel, FormInput} from 'react-native-elements'
+import {ScrollView, View, Picker, Button, Alert} from 'react-native'
+import {Text, FormLabel, FormInput, ListItem} from 'react-native-elements'
+import * as constants from '../constant'
 
 
 
@@ -15,6 +16,8 @@ class ExamWidget extends Component {
             title:  props.navigation.getParam("exam").title,
             description:  props.navigation.getParam("exam").description,
             points:  props.navigation.getParam("exam").points,
+            questionType: 'MC',
+            questions: props.navigation.getParam('exam').questions
         }
 
         this.updateForm = this.updateForm.bind(this);
@@ -44,13 +47,48 @@ class ExamWidget extends Component {
         })
     }
 
+    addQuestion() {
+
+        let questionType = this.state.questionType
+        let question;
+
+        switch (questionType) {
+            case "essay":
+                question = constants.Essay;
+                break;
+            case "truefalse":
+                question = constants.TrueFalse;
+                break;
+            case "choice":
+                question = constants.MultipleChoice;
+                break;
+            case "blanks":
+                question = constants.FillInBlanks;
+                break;
+            default:
+                return;
+        }
+
+        console.log(question)
+
+        return fetch('http://localhost:8080/api/exam/'+this.state.exam.id+'/'+questionType,
+            {
+                headers: {'content-type': 'application/json'},
+                method: "POST",
+                body: JSON.stringify(question)
+            }).then((response) => response.json())
+            .then((question) => {
+                this.setState({questions: [...this.state.questions, question]})
+            })
+    }
+
     render() {
         return (
             <ScrollView>
                 <View style={{padding: 15}}>
 
                     // Edit
-                    <Text h1>Edit</Text>
+                    <Text h3>Edit</Text>
                     <FormLabel>Title</FormLabel>
                     <FormInput
                         value={this.state.title}
@@ -72,8 +110,6 @@ class ExamWidget extends Component {
                             onPress={()=>this.saveExam()}/>
 
 
-
-
                     <View
                         style={{
                             borderBottomColor: 'black',
@@ -81,15 +117,32 @@ class ExamWidget extends Component {
                         }}
                     />
                     // Questions
-                    <Text h1>Questions</Text>
+                    <Text h3>Questions</Text>
+                    {this.state.questions.map( (question, index) => (
+                        <ListItem
+                            key={index}
+                            title={question.title}/>
+                    ))}
 
 
+                    <Text h3>Add Question To Exam</Text>
+                    <Text>{this.state.questionType}</Text>
+                    <Picker
+                        selectedValue={this.state.questionType}
+                        onValueChange={(itemValue, itemIndex) =>
+                            this.setState({questionType: itemValue})}>
+                        <Picker.Item value="choice" label="Multiple choice" />
+                        <Picker.Item value="essay" label="Essay" />
+                        <Picker.Item value="truefalse" label="True or false" />
+                        <Picker.Item value="blanks" label="Fill in the blanks" />
+                    </Picker>
+                    <Button title="Add new question"
+                            color="green"
+                            onPress={() => this.addQuestion()}/>
                 </View>
             </ScrollView>
         )
     }
 }
-
-placeholderFunction = () => {}
 
 export default ExamWidget
